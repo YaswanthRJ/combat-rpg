35ab2419cd803cb46ac9982e371ebed77da9bd57
Yaswanth Raj <yeshwanthraj989@gmail.com>
2026-03-13 20:33:51 +0530

    register routes

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"combat-sim/internal/app"
+	"combat-sim/internal/transport"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Began Combat Engine")
+
+	store := app.NewCampaignStore()
+
+	service := app.NewCampaignService(store)
+
+	handler := transport.NewHandler(service)
+
+	http.HandleFunc("/campaign/start", handler.StartCampaign)
+	http.HandleFunc("/fight/start", handler.StartFight)
+	http.HandleFunc("/fight/action", handler.PerformAction)
+
+	log.Println("Server running on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
